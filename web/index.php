c4ca838e1cb9ea5ba1559cb812f21c437cb04bdc
Dima Pijon <legolaspijon@bigmir.net>
2016-12-12 13:11:50 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
-$updates = file_get_contents('php://input');
-file_put_contents('text.txt', $updates);exit;
+$updates = json_decode(file_get_contents('php://input'), true);
+file_get_contents("https://api.telegram.org/bot260016296:AAH6IbNRAcrhdjGffKxXQiy3fKyzPYUNSLg/sendMessage?chat_id={$updates['message']['chat']['id']}&text=test");
+exit;
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
